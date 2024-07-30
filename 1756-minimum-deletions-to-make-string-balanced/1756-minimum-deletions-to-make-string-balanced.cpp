2fd1dd5dfe0d12d91342bddf149b68e40e3d7ef2
lichenkao <144865753+lichenkao@users.noreply.github.com>
2024-07-31 00:36:01 +0800

    Time: 71 ms (95.69%), Space: 23.7 MB (73.37%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n = s.size();
-        vector<int> ans(n+1, 0);
+        int ans = 0;
         int bCount = 0;
 
         for(int i = 1; i <= n; i++){
             if(s[i-1] == 'b'){
-                ans[i] = ans[i-1];
                 bCount++;
             }
             else{
-                ans[i] = min(ans[i-1] + 1, bCount);
+                ans = min(ans + 1, bCount);
             }
         }
 
-        return ans[n];
+        return ans;
     }
 };
