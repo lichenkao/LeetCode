39f36b606121c1a6a709e14dcbaab0cbad201163
lichenkao <144865753+lichenkao@users.noreply.github.com>
2024-08-19 11:52:42 +0800

    Time: 0 ms (100%), Space: 7.3 MB (80.37%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -3,19 +3,15 @@ public:
     int minSteps(int n) {
         if(n == 1)
             return 0;
-        vector<int> pro;
         int i = 2;
+        int ans = 0;
 
         while(n > 1){
             while(n % i == 0){
-                pro.push_back(i);
                 n /= i;
+                ans += i;
             }
             i++;
-        }
-        int ans = 0;
-        for(int i = 0; i< pro.size(); i++){
-            ans += pro[i];
         }
         return ans;
 
